5c8064c6d6accd262a719425afbb75418d60de80
sava <savamedvedev@gmail.com>
2022-03-19 00:01:28 +0800

    five_commit_4

--- module_4/python lesson6_step4.py
+++ module_4/python lesson6_step4.py
@@ -3,23 +3,26 @@ from selenium.webdriver.common.by import By
 import time
 
 
-link = "http://suninjuly.github.io/find_xpath_form"
-
 try:
+    link = "http://suninjuly.github.io/registration1.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element(by=By.TAG_NAME, value='input')
+    input1 = browser.find_element(by=By.CLASS_NAME, value='first')
     input1.send_keys("Saveliy")
-    input2 = browser.find_element(by=By.NAME, value='last_name')
+    input2 = browser.find_element(by=By.CLASS_NAME, value='second')
     input2.send_keys("Medvedev")
-    input3 = browser.find_element(by=By.CLASS_NAME, value='city')
+    input3 = browser.find_element(by=By.CLASS_NAME, value='third')
     input3.send_keys("Irkutsk")
-    input4 = browser.find_element(by=By.ID, value='country')
-    input4.send_keys("Russia")
-    button = browser.find_element(by=By.XPATH, value="/html/body/div[1]/form/div[6]/button[3]")
+    button = browser.find_element(by=By.CSS_SELECTOR, value="button.btn")
     button.click()
+    time.sleep(1)
+    welcome_text_elt = browser.find_element(by=By.TAG_NAME, value="h1")
+
+    welcome_text = welcome_text_elt.text
+
+    assert "Congratulations! You have successfully registered!" == welcome_text
 
 finally:
-    time.sleep(30)
+    time.sleep(5)
     browser.quit()
